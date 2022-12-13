de91d2152d1372398a3917c738d0bafbd0c4f9dd
Malta112 <jan.gottwald.mtk@icloud.com>
2022-12-13 09:22:56 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo To install Requirements type req, Already installed the requirement? Hit en
 read varmode
 if [ $varmode="req" ]
 then
-sudo apt-get install build-essential gcc make perl dkms -y
+sudo apt-get install build-essential gcc make perl dkms net-tools -y
 sudo reboot
 else
 sudo apt install virtualbox-ext-pack -y
