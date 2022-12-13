485e936d346ca5b4db71a8243d898c70943c8475
Malta112 <jan.gottwald.mtk@icloud.com>
2022-12-13 09:30:01 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo To install Requirements type req, Already installed the requirement? Hit enter
-read varmode
-if [ $varmode="req" ]
+read varmode  #Abfrage der Eingabe
+if [ $varmode="req" ] # Abfrage der Eingabe
 then
-sudo apt-get install build-essential gcc make perl dkms net-tools -y
-sudo reboot
-else
-sudo apt install virtualbox-ext-pack -y
-sudo reboot
+sudo apt-get install build-essential gcc make perl dkms net-tools -y #Installiert build-essential gcc perl dkms net-tools Für die einfache instalation von
+                                                                     #Vitrual Box extension net-tools ist optional aber für die spätere anzeige interessant
+sudo reboot #Startet neu
+else  #Bei eingeben von anderen sachen
+sudo apt install virtualbox-ext-pack -y #installiert ohne nachfrage (-y) die Extension Vorteil braucht die von der VBox gegebenen Disk nicht nachteil benötigt Internet
+sudo reboot #Startet neu, nach neustart funktioniert die Extension
 fi
