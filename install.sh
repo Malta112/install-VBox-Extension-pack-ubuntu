28b4b8ddd6e9c5a1fa8ac1df6394f5c935aea868
Malta112 <jan.gottwald.mtk@icloud.com>
2022-12-13 08:37:31 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+echo To install Requirements type req?
+read varmode
+m =
+if [ $varmode="req" ]
+then
 sudo apt-get install build-essential gcc make perl dkms -y
+sudo reboot
+else
+sudo apt install virtualbox-ext-pack -y
 sudo reboot
